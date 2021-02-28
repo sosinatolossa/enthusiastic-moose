42c964059c6752737469e31177596f75e0871931
sosinatolossa <sosinatolossa16@gmail.com>
2021-02-28 15:12:52 -0600

    modified modules

--- Program.cs
+++ Program.cs
@@ -1,12 +1,77 @@
 ﻿using System;
 
-namespace EnthusiasticMoose
+Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine();
+
+//Let the moose speak!
+MooseSays("H I, I'M  E N T H U S I A S T I C !");
+MooseSays("I really am enthusiastic");
+
+
+// As a question
+bool isTrue = MooseAsks("Is Canada real?");
+Console.WriteLine(isTrue);
+
+void MooseSays(string message)
 {
-    class Program
+    //@ sign means multiple lines
+    Console.WriteLine($@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+}
+
+//true or false function that takes a string type question
+bool MooseAsks(string question)
+{
+    //ask the question with the expectastion of Y/N answer
+    Console.Write($"{question} (Y/N): ");
+    //take what the user enters and change it to lower case
+    string answer = Console.ReadLine().ToLower();
+
+    //while the answer is NOT y or n
+    while (answer != "y" && answer != "n")
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        //keep asking them the question
+        Console.Write($"{question} (Y/N): ");
+        //and still change what's been entered to lower case
+        answer = Console.ReadLine().ToLower();
+    }
+
+    //if the answer is y
+    if (answer == "y")
+    {
+        //return true
+        return true;
+    }
+    else //if not
+    {
+        //return false
+        return false;
     }
 }
